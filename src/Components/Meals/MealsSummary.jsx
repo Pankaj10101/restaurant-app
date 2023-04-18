c33d42c926ebbecbd5e3367faad06a0dc9bf882c
Pankaj10101 <rkay1048@gmail.com>
2023-04-18 23:24:22 +0530

    summary card added

---
+++ src/Components/Meals/MealsSummary.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./MealsSummary.css";
+const MealsSummary = () => {
+  return (
+    <>
+      <section className="summary-card">
+        <h2 className="card-heading">Delicious Food, Delivered To You</h2>
+        <p>
+          Choose your favourite meal from our board section of available meals
+          and emjoy a delicious lunch or dinner at home
+        </p>
+        <p>
+          All our meals are cooked with high-quality ingredients, just-in-time
+          and of course by experienced chefs!
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default MealsSummary;
